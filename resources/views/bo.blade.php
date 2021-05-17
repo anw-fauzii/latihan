ef6647ec1339263e7db54d6b07a52d0ee59199d9
AnwarFauzi123 <anwar.fauzy18@gmail.com>
2021-05-17 10:52:31 +0700

    Menambah file mahasiswa

---
+++ resources/views/bo.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <p> Nama : Anwar Fauzi </p>
+    <p> NIM : 1606028 </p>
+</body>
+</html>
